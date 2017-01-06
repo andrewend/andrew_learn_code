f7fe12a55f5f20bd97ab0dc1ad81b40659be27cd
Andrew Yang <yangbin.end@163.com>
2017-01-06 21:50:30 +0800

    check vrp log size shell
    Signed-off-by: Andrew Yang <yangbin.end@163.com>

---
+++ ch_vrp_log.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+black_list="test.sh ch_vrp_log.sh"
+
+all_files=$(ls -1t *.*)
+
+#echo "yangbin"
+#echo $all_files
+for file in $all_files
+	do
+#		echo $file
+		found=0
+		for black_file in $black_list
+		do
+#			echo $black_file
+			if test "$file" = "$black_file" 
+			then
+				found=1
+				break
+			fi
+		done
+
+		if test $found -eq 0
+		then
+			echo "not found $file"
+		else
+			echo "found $file"
+#			rm $file
+		fi
+done
